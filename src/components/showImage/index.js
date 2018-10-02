3c9ebb62e0cf1dfeef05eac6ddc145e6e72ecb3d
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-02 12:21:03 +0200

    add unit test showImage

--- src/components/showImage/index.js
+++ src/components/showImage/index.js
@@ -1,36 +1,3 @@
-import { h, Component } from 'preact';
-import PropTypes from 'prop-types';
-
-class ShowImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showImage: false
-    }
-  }
-
-  render(props, state) {
-    const { showImage } = state;
-    const { imageUrl } = props;
-    return (
-      <div>
-        <h2>Show image</h2>
-        {
-          showImage &&
-            <img 
-              src={imageUrl}
-              alt="test image" 
-              style={{ with: '100px', height: '100px', display: 'block', marginBottom: '10px' }} 
-            />
-        }
-        <button onClick={() => this.setState({ showImage: true })} >Show image</button>
-      </div>
-    )
-  }
-}
-
-ShowImage.propTypes = {
-  imageUrl: PropTypes.string.isRequired
-};
+import ShowImage from './ShowImage';
 
 export default ShowImage;
--- src/components/showImage/showImage.test.js
+++ src/components/showImage/showImage.test.js
@@ -1,9 +1,23 @@
-import ShowImage from './index';
+import ShowImage from '.';
 import { shallow } from 'preact-render-spy';
 
 describe('Show image test', () => {
   it('Has a show image button', () => {
     const context = shallow(<ShowImage url="https://pbs.twimg.com/profile_images/941288498602049536/DeDdMSEx_400x400.jpg" />);
     expect(context.find('button').length).toBe(1);
+  });
+
+  it('Doesnt show an image by default', () => {
+    const context = shallow(<ShowImage url="https://pbs.twimg.com/profile_images/941288498602049536/DeDdMSEx_400x400.jpg" />);
+    expect(context.find('img').length).toBe(0);
+  });
+
+  it('Hides and shows the image', () => {
+    const context = shallow(<ShowImage url="https://pbs.twimg.com/profile_images/941288498602049536/DeDdMSEx_400x400.jpg" />);
+    expect(context.find('img').length).toBe(0);
+    context.find('button').simulate('click');
+    expect(context.find('img').length).toBe(1);
+    context.find('button').simulate('click');
+    expect(context.find('img').length).toBe(0);
   });
 });
