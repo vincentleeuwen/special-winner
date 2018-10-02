58c5f6483e931fc568a3c12b619bbc868a4d720e
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-02 11:30:19 +0200

    add time to stuff component

--- src/routes/stuff/index.js
+++ src/routes/stuff/index.js
@@ -1,10 +1,13 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-const Stuff = () => (
-  <div style={{ padding: '150px' }}>
-    <h2>Stuff</h2>
-    <p>Lorem ipsum dolorem.</p>
-  </div>
-);
+const Stuff = () => {
+  let time = new Date().toLocaleTimeString();
+  return (
+    <div style={{ padding: '150px' }}>
+      <h2>Stuff</h2>
+      <p>Time: <strong>{ time }</strong>.</p>
+    </div>
+  );
+}
 
 export default Stuff;
