399500cdb3d7e8cf0384df14453d0a7a43f22b08
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-10-02 12:11:38 +0200

    add simple showImage component

--- src/routes/clock/index.js
+++ src/routes/clock/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import ShowImage from '../../components/showImage';
 
 export default class Clock extends Component {
   constructor() {
@@ -23,6 +24,10 @@ export default class Clock extends Component {
       <div style={{ padding: '150px' }}>
         <h2>Clock</h2>
         <p>Time: <strong>{ time }</strong>.</p>
+        <br />
+        <ShowImage
+          imageUrl="https://pbs.twimg.com/profile_images/941288498602049536/DeDdMSEx_400x400.jpg"
+        />
       </div>
     );
   }
